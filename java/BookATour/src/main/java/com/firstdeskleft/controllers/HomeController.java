68ce6d7ad6cb1308613cd4fc84384c499f4fb50a
Tasos Lelakis <tasospatra@hotmail.com>
2020-01-17 16:29:39 +0200

    added method getPrincipal

--- java/BookATour/src/main/java/com/firstdeskleft/controllers/HomeController.java
+++ java/BookATour/src/main/java/com/firstdeskleft/controllers/HomeController.java
@@ -2,6 +2,9 @@ package com.firstdeskleft.controllers;
 
 import com.firstdeskleft.entities.User;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +25,7 @@ public class HomeController {
 
     @GetMapping("/HomeAdmin")
     public String adminHome() {
-
+        getPrincipal();
         return "HomeAdmin";
     }
 
@@ -38,6 +41,18 @@ public class HomeController {
         return "HomeCustomer";
     }
 
-   
-
+    private UserDetails getPrincipal(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Object o = auth.getPrincipal();
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+        if(o instanceof User){
+            System.out.println("o=user");
+        }else{
+            System.out.println("o!=user");
+        }
+        System.out.println("User has username: " + userDetails.getUsername());
+        System.out.println("User has authorities: " + userDetails.getAuthorities());
+        return userDetails;
+    }
+    
 }
